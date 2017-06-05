378b85dd59ecc79536fe30d23730e67a225d25a0
Fahmi Akbar <fahmialiasakbar@gmail.com>
2017-06-05 16:05:15 +0700

    Finish commit

---
+++ app/src/main/java/layout/FotoFragment.java
@@ -0,0 +1,30 @@
+package layout;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import xir1n11.remidiuts.telkomschid.nav1.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class FotoFragment extends Fragment {
+
+
+    public FotoFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_foto, container, false);
+    }
+
+}
